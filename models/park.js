abefacece070e6b144ab6cd2d7d0aae3642fbf23
Andrew Gray <Andrew.rhysgray@hotmail.co.uk>
2020-01-12 23:16:04 +0000

    second commit, initalised park name, ticket_price, and amount of dinosaurs

--- models/park.js
+++ models/park.js
@@ -1,5 +1,9 @@
-const Park = function (name, ticket_price, dinosaur_collection ) {
+const Park = function (name, ticket_price, dinosaurs ) {
+    this.name = name;
+this.ticket_price = ticket_price;
 this.dinosaurs = dinosaurs;
+
+
  };
 
  Park.prototype.showName = function () {
