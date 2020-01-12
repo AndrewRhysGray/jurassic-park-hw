63a8041973f0c106d2b68104a664681278ffb6e2
Andrew Gray <Andrew.rhysgray@hotmail.co.uk>
2020-01-12 21:44:45 +0000

    first commit, tests are passing but currently setting up parameters for the park

---
+++ models/park.js
@@ -0,0 +1,10 @@
+const Park = function (name, ticket_price, dinosaur_collection ) {
+this.dinosaurs = dinosaurs;
+ };
+
+ Park.prototype.showName = function () {
+     this.dinosaurs.name
+
+     const nameArray = this.dinosaurs.map(park => dinosaur.name)
+  return nameArray;
+ };
---
+++ specs/park_spec.js
@@ -0,0 +1,51 @@
+const assert = require('assert');
+const Park = require('../models/park.js');
+const Dinosaur = require('../models/dinosaur.js');
+
+describe('Park', function() {
+  let dino1;
+  let dino2;
+  let dino3;
+  let dino4;
+  let dino5;
+  let dino6;
+  let dino7;
+
+  beforeEach(function () {
+    dino1 = new Dinosaur('dino1', 'carnivore', 100);
+    dino2 = new Dinosaur('dino2', 'carnivore', 150);
+    dino3 = new Dinosaur('dino3', 'carnivore', 200,);
+    dino4 = new Dinosaur('dino4', 'carnivore', 180);
+    dino5 = new Dinosaur(' dino5', 'carinivore', 190);
+
+    dinosaurs = [dino1 , dino2, dino3, dino4, dino5];
+    park = new Park(dinosaurs);
+
+  });
+
+
+
+  it('should have a name', function () {
+  const actual = park.name;
+    assert.StrictEqual(actual, park.name);
+  });
+
+  it('should have a ticket price');
+
+  it('should have a collection of dinosaurs');
+
+  it('should be able to add a dinosaur to its collection');
+
+  it('should be able to remove a dinosaur from its collection');
+
+  it('should be able to find the dinosaur that attracts the most visitors');
+
+  it('should be able to find all dinosaurs of a particular species');
+
+  it('should be able to calculate the total number of visitors per day');
+
+  it('should be able to calculate the total number of visitors per year');
+
+  it('should be able to calculate total revenue for one year');
+
+});
